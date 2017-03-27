c71a100e7632e157c522ed9ea25283bd9724d72a
carangel <yaronmartin@gmail.com>
2017-03-27 16:52:45 +0300

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -5,4 +5,4 @@ docker rm bitcoin-segwit-uasf
 docker run -it 	--name bitcoin-segwit-uasf \
 				-p 8333:8333 \
 				-v <path_to_local_bitcoin_data_folder>:/home/bitcoin \
-                <name_of_the_docker_image>
+                		<name_of_the_docker_image>
